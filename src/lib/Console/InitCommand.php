9dde72b5cbe057192b3a59f3841c85b15c7234b3
Taylor Otwell <taylorotwell@gmail.com>
2014-03-03 18:47:41 -0600

    Release.

---
+++ src/lib/Console/InitCommand.php
@@ -0,0 +1,49 @@
+<?php namespace Laravel\Envoy\Console;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitCommand extends \Symfony\Component\Console\Command\Command {
+
+	use Command;
+
+	/**
+	 * Configure the command options.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this->setName('init')
+                  ->setDescription('Create a new Envoy file in the current directory.')
+                  ->addArgument('host', InputArgument::REQUIRED, 'The host server to initialize with.');
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return void
+	 */
+	protected function fire()
+	{
+		if (file_exists(getcwd().'/Envoy.blade.php'))
+		{
+			$this->output->writeln('<error>Envoy file already exists!</error>');
+
+			return;
+		}
+
+		file_put_contents(getcwd().'/Envoy.blade.php', "@servers(['web' => '".$this->input->getArgument('host')."'])
+
+@task('deploy')
+	cd /path/to/site
+	git pull origin master
+@endtask
+");
+
+		$this->output->writeln('<info>Envoy file created!</info>');
+	}
+
+}
