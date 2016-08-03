87294abc26551ceb80682f842229a102bc82f9ec
Taylor Otwell <taylorotwell@gmail.com>
2016-08-03 12:37:02 -0500

    change default envoy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,13 +1,5 @@
-@servers(['web' => '192.168.1.1'])
+@servers(['web' => ['192.168.1.1']])
 
 @task('foo', ['on' => 'web'])
     ls -la
 @endtask
-
-@task('confirmed', ['on' => 'web', 'confirm' => true])
-    ls -la
-@endtask
-
-@after
-    @hipchat('token', 'room', 'Envoy')
-@endafter
