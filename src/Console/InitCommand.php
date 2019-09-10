64263db82d5fc0c47c7b28c7f6d422ef5b250160
Dries Vints <dries.vints@gmail.com>
2019-09-10 17:44:44 +0200

    Apply fixes from StyleCI (#159)

--- src/Console/InitCommand.php
+++ src/Console/InitCommand.php
@@ -2,8 +2,8 @@
 
 namespace Laravel\Envoy\Console;
 
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\InputArgument;
 
 class InitCommand extends SymfonyCommand
 {
