ce80dfbd55be21bdaef1b0c5a01bcc38524a0e87
Simon Hamp <simon.hamp@me.com>
2021-02-26 16:13:23 +0000

    Add success callback support

--- src/TaskContainer.php
+++ src/TaskContainer.php
@@ -36,6 +36,13 @@ class TaskContainer
      * @var array
      */
     protected $tasks = [];
+    
+    /**
+     * All of the "success" callbacks.
+     *
+     * @var array
+     */
+    protected $success = [];
 
     /**
      * All of the "error" callbacks.
@@ -479,6 +486,27 @@ class TaskContainer
     public function getFinishedCallbacks()
     {
         return $this->finished;
+    }
+    
+    /**
+     * Register an success-task callback.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function success(Closure $callback)
+    {
+        $this->success[] = $callback;
+    }
+
+    /**
+     * Get all of the success-task callbacks.
+     *
+     * @return array
+     */
+    public function getSuccessCallbacks()
+    {
+        return $this->success;
     }
 
     /**
