6d89e803a9d1a92d68a9bf24e1de8f32a7fda25a
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-02-04 15:50:27 -0500

    Add `@before` hook

--- tests/CompilerTest.php
+++ tests/CompilerTest.php
@@ -19,4 +19,17 @@ EOL;
 
         $this->assertSame(1, preg_match('/\$__container->finished\(.*?\}\);/s', $result, $matches));
     }
+
+    public function test_compile_before_statement()
+    {
+        $str = <<<'EOL'
+@before
+    echo "Running {{ $task }} task.";
+@endbefore
+EOL;
+        $compiler = new Compiler();
+        $result = $compiler->compile($str);
+
+        $this->assertSame(1, preg_match('/\$__container->before\(.*?\}\);/s', $result, $matches));
+    }
 }
