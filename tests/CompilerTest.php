6829791810a6022fc9dad9694398fd3a63a28e60
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-02-04 14:43:02 -0500

    Add baseline test for compiling servers statement

--- tests/CompilerTest.php
+++ tests/CompilerTest.php
@@ -19,4 +19,18 @@ EOL;
 
         $this->assertSame(1, preg_match('/\$__container->finished\(.*?\}\);/s', $result, $matches));
     }
+
+    public function test_compile_servers_statement()
+    {
+        $str = <<<'EOL'
+@servers(['local' => '127.0.0.1', 'remote' => '1.1.1.1'])
+EOL;
+        $compiler = new Compiler();
+        $result = $compiler->compile($str);
+
+        $this->assertStringContainsString(
+            "\$__container->servers(['local' => '127.0.0.1', 'remote' => '1.1.1.1']);",
+            $result
+        );
+    }
 }
