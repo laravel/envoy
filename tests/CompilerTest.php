b1992510501c171df2ad1914890f1d3c38007004
Jacob Baker-Kretzmar <bakerkretzmar@users.noreply.github.com>
2022-02-06 17:42:22 -0500

    Revert "Allow line breaks inside `@server` statements (#241)" (#243)
    This reverts commit 4c4725571b55fe620d341716b71e3387cec717a4.

--- tests/CompilerTest.php
+++ tests/CompilerTest.php
@@ -20,37 +20,6 @@ EOL;
         $this->assertSame(1, preg_match('/\$__container->finished\(.*?\}\);/s', $result, $matches));
     }
 
-    public function test_compile_servers_statement()
-    {
-        $str = <<<'EOL'
-@servers(['local' => '127.0.0.1', 'remote' => '1.1.1.1'])
-EOL;
-        $compiler = new Compiler();
-        $result = $compiler->compile($str);
-
-        $this->assertStringContainsString(
-            "\$__container->servers(['local' => '127.0.0.1', 'remote' => '1.1.1.1']);",
-            $result
-        );
-    }
-
-    public function test_compile_servers_statement_with_line_breaks()
-    {
-        $str = <<<'EOL'
-@servers([
-    'local' => '127.0.0.1',
-    'remote' => '1.1.1.1',
-])
-EOL;
-        $compiler = new Compiler();
-        $result = $compiler->compile($str);
-
-        $this->assertStringContainsString(
-            "\$__container->servers([\n    'local' => '127.0.0.1',\n    'remote' => '1.1.1.1',\n]);",
-            $result
-        );
-    }
-
     public function test_compile_before_statement()
     {
         $str = <<<'EOL'
